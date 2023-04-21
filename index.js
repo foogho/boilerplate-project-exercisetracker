9632fd1ce5552ef03b9eb2bacad0b120bfd9ea50
Ali Ebrahimi <aliebrahimi2150@gmail.com>
2023-04-21 16:41:18 +0330

    feat: add db connection

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const mongoose = require('mongoose');
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log('db connected!');
 });
 
-
-
-
+app.use(cors());
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log('Your app is listening on port ' + listener.address().port);
+});
