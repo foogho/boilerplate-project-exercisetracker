91169d17eec26a6cb5dc214e7dffab0a1e488f75
Ali Ebrahimi <aliebrahimi2150@gmail.com>
2023-04-22 15:12:46 +0330

    feat: add post exercises endpoint

--- routes/api.js
+++ routes/api.js
@@ -16,4 +16,25 @@ router.post('/users', async (req, res, next) => {
   }
 });
 
+router.post('/users/:id/exercises', async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const body = req.body;
+    const user = await User.findById(userId);
+    const { description, duration, date } = body;
+    const exercise = user.log.create({ description, duration, date });
+    user.log.push(exercise);
+    await user.save();
+    res.json({
+      username: user.username,
+      description,
+      duration,
+      date,
+      _id: exercise.id,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
